08267804cd1123a04d362ad2b52af9b4c86a8a97
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-11-16 16:18:49 +0100

    added api url

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,8 @@ import {store} from './store';
     data(){
       return{
         store,
+        endPointMovie: 'movie?api_key=6e5beca259c47107979aef3b3305cc9a&language=en-US&page=1&query=a',
+        endPointTvSeries: 'tv?api_key=6e5beca259c47107979aef3b3305cc9a&language=en-US&page=1&query=a',
       }
     }
 }
--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-   
+   apiURL: 'https://api.themoviedb.org/3/',
+   MovieList: [],
+   TvList: [],
 });
